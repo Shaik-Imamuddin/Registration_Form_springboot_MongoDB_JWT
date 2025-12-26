7318c51e01a46d669e4bbd8345e889896d364140
Immu <shaikimam37302@gmail.com>
2025-12-26 09:16:29 +0530

    Registration Form with JWT Auth

---
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.backend.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.backend.model.User;
+
+public interface UserRepository extends MongoRepository<User, String> { }
